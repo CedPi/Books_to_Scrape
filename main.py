56e548cd32b7f6aad5bf043c7a4df2a0fc426d95
Cedric Pinson <a.cedric.pinson@gmail.com>
2022-02-04 01:59:13 +0100

    Modification du code permettant le scrapping de toutes les catégories (Ajout de la fonction get_categories et boucle sur les catégories dans le programme principal)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import re
 import requests
@@ -27,10 +28,8 @@ HEADER = [
     'image_url'
 ]
 
-all_data = []
 
-
-def scrap_one_product(home: str, url: str, ratings: list) -> list:
+def scrap_one_product(home: str, url: str, category: str, ratings: list) -> list:
     """Récupération des infos d'un produit via url"""
     data = []
     page = requests.get(url)
@@ -44,7 +43,9 @@ def scrap_one_product(home: str, url: str, ratings: list) -> list:
     # upc
     data.append(tds[0].string)
     # title
-    data.append(soup.find('div', class_='product_main').find('h1').string)
+    title = soup.find('div', class_='product_main').find('h1').string
+    data.append(title)
+    print('\t\t' + title)
     # price including tax
     data.append(tds[2].string)
     # price excluding tax
@@ -53,9 +54,13 @@ def scrap_one_product(home: str, url: str, ratings: list) -> list:
     number = re.findall(r"\d+", tds[5].string)
     data.append(number[0])
     # product description
-    data.append(soup.find(id='product_description').find_next('p').string)
+    try:
+        data.append(soup.find(id='product_description').find_next('p').string)
+    except:
+        data.append('')
+        print("\t\t\t(No description found for : " + title + ')')
     # category
-    data.append(soup.find('ul', class_='breadcrumb').find_all('li')[2].find('a').string)
+    data.append(category)
     # review rating
     data.append(ratings[soup.find('p', class_='star-rating').attrs['class'][1]])
     # image url
@@ -64,28 +69,60 @@ def scrap_one_product(home: str, url: str, ratings: list) -> list:
     return data
 
 
-def save_to_csv(csv_header: list, data: list):
-    with open('one_category.csv', 'w', newline='') as file:
+def save_to_csv(csv_header: list, data: list, category: str):
+    categories_path = os.path.curdir + '/categories'
+    if not os.path.isdir(categories_path):
+        os.mkdir('categories')
+    csv_path = 'categories/' + category + '.csv'
+
+    with open(csv_path, 'w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file, delimiter=',')
         writer.writerow(csv_header)
         for d in data:
             writer.writerow(d)
 
 
-page_contains_book = True
-page_number = 1
-while page_contains_book:
-    page = requests.get('https://books.toscrape.com/catalogue/category/books/fiction_10/page-' + str(page_number) + '.html')
-    if page.status_code != 200:
-        break
+def get_categories(home_url: str) -> list:
+    categories = dict()
+    page = requests.get(home_url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    products = soup.find_all('article', class_='product_pod')
-    
-    for product in products:
-        product_url = HOME + product.find('a').attrs['href'].replace('../../../', '/catalogue/')
-        one_data = scrap_one_product(HOME, product_url, RATINGS)
-        all_data.append(one_data)
+    side_categories = soup.find('div', class_='side_categories').find_all('a')
+    for side_cat in side_categories:
+        cat_name = side_cat.string.strip()
+        if cat_name != 'Books':
+            categories[cat_name] = side_cat.attrs['href'].strip()
+    return categories
 
-    save_to_csv(HEADER, all_data)
 
-    page_number += 1
+
+# Main program
+categories = get_categories(HOME)
+for cat_name, cat_url in categories.items():
+    print('\n' + cat_name.upper())
+    all_data = []
+    page_number = 1
+    cat_url_wo_index = cat_url.replace('/index.html', '')
+    working_url = HOME + '/' + cat_url
+
+    while True:
+        page = requests.get(working_url)
+        if page.status_code != 200:
+            break
+
+        print('\t[ Page ' + str(page_number) + ' : ' + working_url + ' ]')
+        soup = BeautifulSoup(page.content, 'html.parser')
+        number_of_products = int(soup.find('form', class_='form-horizontal').find_all('strong')[0].string)
+        products = soup.find_all('article', class_='product_pod')
+        
+        for product in products:
+            product_url = HOME + product.find('a').attrs['href'].replace('../../../', '/catalogue/')
+            one_data = scrap_one_product(HOME, product_url, cat_name, RATINGS)
+            all_data.append(one_data)
+
+        if number_of_products > 20:
+            page_number += 1
+            working_url = HOME + '/' + cat_url_wo_index + '/page-' + str(page_number) + '.html'
+        else:
+            break
+
+    save_to_csv(HEADER, all_data, cat_name)
